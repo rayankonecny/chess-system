c2fe087e0f982822a0bb3777229f24223e3511b3
Rayan <rayanvix@gmail.com>
2022-08-18 13:22:10 -0300

    Comentario

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,9 +5,10 @@ import boardgame.Board;
 public class Program {
 
 	public static void main(String[] args) {
-
+      // add
 	  Board board = new Board(8,8);
       System.out.println(board);
 	}
 
 }
+ 
