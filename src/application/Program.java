6508c91732ba23c8455c308cdc08ec1ab99e2932
Rayan <rayanvix@gmail.com>
2022-08-22 10:53:59 -0300

    Chess

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,12 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-      // add
-	  Board board = new Board(8,8);
-      System.out.println(board);
+      ChessMatch chessmatch = new ChessMatch();
+      UI.printBoard(chessmatch.getPieces());      
 	}
 
 }
